852891c244215455c92aa0a131cc23b6e2eb8a86
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-03-18 16:05:05 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-alert('hhh')
+let n = prompt('введи свій вік');
+
+
+if (n < 0) {
+    alert('ти ще не народився!');
+} else if (n <= 6) {
+    alert('тобі завтра в садок');
+} else if (n <= 16) {
+    alert('тобі завтра в школу');
+} else if (n <= 24) {
+   alert('тобі завтра в університет');
+} else if (n <= 25) {
+    alert('тобі завтра на роботу');
+} else if (n <= 60) {
+    alert('ти вже пенсіонер');
+} else if (n <= 100) {
+    alert('ти живий?');
+} else {
+    alert('шось я непоняв то');
+}  
+function uuu(){
+
+    let m = prompt('ведіть вік для реєстрації')
+
+    if (m > 18){
+        var url = "https://github.com/wrouruy/homework";
+        window.location.assign(url);
+    }
+}
+ 
