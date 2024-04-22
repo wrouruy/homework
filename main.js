19b0798075d25d954fcad1e3de182c211eac0fa2
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-04-22 17:30:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-function github(){
-    location.replace('https://github.com/wrouruy/homework')
-}
-console.error('no function')
-let yearOne = 2024;
-let leapYear = (yearOne % 4 === 0 && yearOne % 100 !== 0) || (yearOne % 400 === 0);
-console.log(leapYear);
-
-
-console.error('with function')
-function isLeapYear(year) {
-    return (yearTwo % 4 === 0 && yearTwo % 100 !== 0) || (yearTwo % 400 === 0);
+console.error('task 12:')
+function calcDiscrim(a, b, c) {
+    let discriminant = b * b - 4 * a * c;
+    return discriminant;
 }
 
-const yearTwo = 2024;
-console.log(isLeapYear(yearTwo));
+let a = 2;
+let b = 6;
+let c = 4;
+let discriminant = calcDiscrim(a, b, c);
+console.log(`дискримінант дорівнює: ${discriminant}`);
+
+
+console.error('task 13:')
+function letQuadratic(a, b, c) {
+    let D = b * b - 4 * a * c;
+  
+    if (D > 0) {
+      let x1 = (- b + Math.sqrt(D)) / (2 * a);
+      let x2 = (- b - Math.sqrt(D)) / (2 * a);
+      return ( x1, x2 );
+    } else if (D === 0) {
+      let x = -b / (2 * a);
+      return ( x );
+    }
+  }
+  console.log('чесно, я не знаю як вона робить, і чи повина вона робити вот-так, ну але по коді все має бути логічно')
+  let result = letQuadratic(-5, 6, 9);
+  console.log(result);
+  
+
+
+
+function button(){
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
