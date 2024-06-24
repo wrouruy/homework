e392755016464b6700e0731624a39d12c6b9ccb2
wrouruy <161200383+wrouruy@users.noreply.github.com>
2024-06-24 12:22:43 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,93 @@
-console.error('task 12:')
-function calcDiscrim(a, b, c) {
-    let discriminant = b * b - 4 * a * c;
-    return discriminant;
-}
+// подія заходу
+$('.asistantPreview').hide(0);
+$('.asistantNumber').hide(0);
+$('.asistantMessage').hide(0);
+$('.alertError').hide(0);
 
-let a = 2;
-let b = 6;
-let c = 4;
-let discriminant = calcDiscrim(a, b, c);
-console.log(`дискримінант дорівнює: ${discriminant}`);
+$('.asistantMessage').append(`<div class='subscriberMessageItem'><p style='margin-left: 10px'>Добрий день! Мене звати Олег, вам з чимось допомогти або підсказати?<span class="miniText">${new Date().getHours()}:${new Date().getMinutes()}</span><p></div>`);
 
 
-console.error('task 13:')
-function letQuadratic(a, b, c) {
-    let D = b * b - 4 * a * c;
-  
-    if (D > 0) {
-      let x1 = (- b + Math.sqrt(D)) / (2 * a);
-      let x2 = (- b - Math.sqrt(D)) / (2 * a);
-      return ( x1, x2 );
-    } else if (D === 0) {
-      let x = -b / (2 * a);
-      return ( x );
+
+let closeHide = false
+$('.asistantButton').click(function () {
+    $('.asistantPreview').slideToggle(300);
+    $('.asistantNumber').fadeOut(300);
+    $('.alertError').fadeOut(300);
+    leftBtn = 0
+    rightBtn = 0
+
+
+    if(closeHide == false){
+        $('.asistantMessage').css('right', '315px');
+        $('#send').css('right', '321px');
+        closeHide = true
+    } else{
+        $('.asistantMessage').css('right', '100px');
+        $('#send').css('right', '100px');
+        closeHide = false
     }
-  }
-  console.log('чесно, я не знаю як вона робить, і чи повина вона робити вот-так, ну але по коді все має бути логічно')
-  let result = letQuadratic(-5, 6, 9);
-  console.log(result);
-  
+    if(closeHide == false){
+        closeHide = true
+    } else if(closeHide == true){
+        closeHide = false
+    }
+})
+
+let leftBtn = 0
+let rightBtn = 0
+$('.call').click(function() {
+    $('.alertError').toggle(300);
+    leftBtn++
+    if(leftBtn >= 1 && rightBtn >= 1){
+        $('#textP').text('Воно тоже недоступно (')
+    } else{
+        $('#textP').text('Ця функція наразі недоступна :(')
+    }
+})
+$('.sendExploes').click(function() {
+    $('.alertError').toggle(300);
+    rightBtn++
+    if(leftBtn >= 1 && rightBtn >= 1){
+        $('#textP').text('Воно тоже недоступно (')
+    } else{
+        $('#textP').text('Ця функція наразі недоступна :(')
+    }
+})
+
+$('#send').click(function () {
 
 
+    $('.asistantMessage').append(`<div class='subscriberMessageItem'><p style='margin-left: 10px'>${$('#message').val()}
+    <span class="miniText">${new Date().getHours()}:${new Date().getMinutes()}</span><p>
+    </div>`);
+    $('.asistantMessage').append(`<p style="color: red; margin-left: 10px">помилка <span style="font-weight: 700; color: red">0052</span>: IQ олега не витримало такого напору.</p>`);
+    message.value = ''
+})
 
-function button(){
-    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
-}
+$('.wrap').click(function() {
+    $('.asistantPreview').slideUp(300);
+    $('.asistantNumber').fadeOut(300);
+    $('.alertError').fadeOut(300);
+
+    $('.asistantMessage').css('right', '100px');
+    $('#send').css('right', '100px');
+    leftBtn = 0
+    rightBtn = 0
+})
+
+$('#goTelegram').click(function() {
+    window.location.href = 'https://t.me/letfin2';
+});
+
+$('#goNumber').click(function () {
+    $('.asistantNumber').slideToggle(300);
+})
+
+$('#goTalk').click(function() {
+    $('.asistantMessage').slideToggle(300);
+});
+
+
+// let currentTime = new Date();
+
+// console.log(currentTime);
